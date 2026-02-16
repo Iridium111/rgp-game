57de1dd884b615c21df223f167fb91273dece8fd
Евгений <pasqualepogboom@gmail.com>
2026-02-16 21:24:28 +0300

    Добавил инвентарь, вещи теперь храняться в нем и можно надеть на героя

--- main.py
+++ main.py
@@ -15,64 +15,101 @@ class Characteristic:
 
 
 class Armor:
-    def __init__(self, name, characteristic, value, slot):
+    def __init__(self, name, category, strength=None, value_strength=None, agility=None, value_agility=None,
+                 intellect=None, value_intellect=None):
         self.name = name
-        self.characteristic = characteristic
-        self.value = value
-        self.slot = slot
+        self.category = category
+        self.strength = strength
+        self.agility = agility
+        self.intellect = intellect
+        self.value_strength = value_strength
+        self.value_agility = value_agility
+        self.value_intellect = value_intellect
 
 
 class Helmet(Armor):
-    def __init__(self, name, characteristic, value):
-        super().__init__(name, characteristic, value, slot="helmet")
+    def __init__(self, name, category, strength=None, value_strength=None, agility=None, value_agility=None,
+                 intellect=None, value_intellect=None):
+        super().__init__(name, category, strength, value_strength, agility, value_agility, intellect,
+                         value_intellect)
 
     def __repr__(self):
-        return f'{self.name}, {self.characteristic} {self.value}\n'
+        return (f'{self.category} {self.name} Характеристики:\n{self.strength}:{self.value_strength}\n'
+                f'{self.agility}:{self.value_agility}\n{self.intellect}:{self.value_intellect}\n')
 
 
 class Chestplate(Armor):
-    def __init__(self, name, characteristic, value):
-        super().__init__(name, characteristic, value, slot="chestplate")
+    def __init__(self, name, category, strength=None, value_strength=None, agility=None, value_agility=None,
+                 intellect=None, value_intellect=None):
+        super().__init__(name, category, strength, value_strength, agility, value_agility, intellect,
+                         value_intellect)
 
     def __repr__(self):
-        return f'{self.name}, {self.characteristic} {self.value}\n'
+        return (f'{self.category} {self.name} Характеристики:\n{self.strength}:{self.value_strength}\n'
+                f'{self.agility}:{self.value_agility}\n{self.intellect}:{self.value_intellect}\n')
 
 
 class Greaves(Armor):
-    def __init__(self, name, characteristic, value):
-        super().__init__(name, characteristic, value, slot="greaves")
+    def __init__(self, name, category, strength=None, value_strength=None, agility=None, value_agility=None,
+                 intellect=None, value_intellect=None):
+        super().__init__(name, category, strength, value_strength, agility, value_agility, intellect,
+                         value_intellect)
 
     def __repr__(self):
-        return f'{self.name}, {self.characteristic} {self.value}\n'
+        return (f'{self.category} {self.name} Характеристики:\n{self.strength}:{self.value_strength}\n'
+                f'{self.agility}:{self.value_agility}\n{self.intellect}{self.value_intellect}\n')
 
 
 class Boots(Armor):
-    def __init__(self, name, characteristic, value):
-        super().__init__(name, characteristic, value, slot="boots")
+    def __init__(self, name, category, strength=None, value_strength=None, agility=None, value_agility=None,
+                 intellect=None, value_intellect=None):
+        super().__init__(name, category, strength, value_strength, agility, value_agility, intellect,
+                         value_intellect)
 
     def __repr__(self):
-        return f'{self.name}, {self.characteristic} {self.value}\n'
+        return (f'{self.category} {self.name} Характеристики:\n{self.strength}:{self.value_strength}\n'
+                f'{self.agility}:{self.value_agility}\n{self.intellect}:{self.value_intellect}\n')
 
+
+class Inventory:
+    def __init__(self):
+        self.slots_category = {}
+        self.slots_items = {}
+
+    def add_in_inventory(self, item):
+        """Добавление предметов в инвентарь"""
+        # Сохраняем по имени
+        self.slots_items[item.name] = item
+
+        # Сохраняем по категориям
+        if item.category not in self.slots_category:
+            self.slots_category[item.category] = {}     # создаем словарь для этой категории
+        self.slots_category[item.category][item.name] = item    # кладем предмет в свою категорию
 
 class Hero:
     def __init__(self, name, weapon):
         self._name = name
-        self._characteristic = Characteristic(100, 100, self,)   #self - для передачи характеристик в Characteristic
+        self._characteristic = Characteristic(100, 100, self)   #self - для передачи характеристик в Characteristic
         self._weapon = weapon
         self.BASE_HEALTH = 100
         self.slots_armor = {
-            'helmet': None,
-            'chestplate': None,
-            'greaves': None,
-            'boots': None,
+            'Шлем': None,
+            'Нагрудник': None,
+            'Поножи': None,
+            'Ботинки': None,
         }
         self.amount_strength = 0
         self.health = 0
 
-    def equip_armor(self, item):
+    def equip_armor(self, inventory, key, name_thing):
         """Одевание брони"""
-        self.slots_armor[item.slot] = item
-        self.amount_strength += item.value
+        if key.title() in inventory.slots_category:
+            item = inventory.slots_category[key][name_thing]
+            self.slots_armor[key] = item
+
+        else:
+            print(f'Нема такова')
+
 
     def calculation_health(self):
         """Подсчет здоровья"""
@@ -92,19 +129,25 @@ class Hero:
 
 
 weapon_1 = Weapon('Фростморн', 50, 'Меч')
+crown = Helmet('Шлем Господства', 'Шлем', 'Сила', 5, 'Ловкость', 7,  'Интеллект', 3)
+chestplate = Chestplate('Шипастый латный нагрудник', 'Нагрудник', 'Сила', 12, 'Ловкость', 15, 'Интеллект', 5)
+greaves = Greaves('Шипастые латные поножи', 'Поножи', 'Сила', 10, 'Ловкость', 12, 'Интеллект', 4)
+boots = Boots('Латные ботинки короля мертвых', 'Ботинки', 'Сила', 6, 'Ловкость', 11, 'Интеллект', 3)
+#
+inventory_hero = Inventory()
+inventory_hero.add_in_inventory(crown)
+inventory_hero.add_in_inventory(chestplate)
+inventory_hero.add_in_inventory(greaves)
+inventory_hero.add_in_inventory(boots)
 
 hero = Hero('Артас', weapon_1)
 hero.attack()
 
-crown = Helmet('Шлем Господства', 'сила', 5)
-chestplate = Chestplate('Шипастый латный нагрудник', 'сила', 10)
-greaves = Greaves('Шипастые латные поножи', 'сила', 4)
-boots = Boots('Латные ботинки короля мертвых', 'сила', 3)
+hero.equip_armor(inventory_hero, 'Шлем', 'Шлем Господства')
+hero.equip_armor(inventory_hero, 'Нагрудник', 'Шипастый латный нагрудник')
+hero.equip_armor(inventory_hero, 'Поножи', 'Шипастые латные поножи')
+hero.equip_armor(inventory_hero, 'Ботинки', 'Латные ботинки короля мертвых')
 
-hero.equip_armor(crown)
-hero.equip_armor(chestplate)
-hero.equip_armor(greaves)
-hero.equip_armor(boots)
 hero.calculation_health()
 
 print(hero.slots_armor)
